db5981e9d1afac1a9cdf64ea94dc09763c732a8d
Stazzy <freef@tutamail.com>
2025-01-02 23:47:26 -0500

    Add files via upload

---
+++ Random.js
@@ -0,0 +1,1 @@
+var sites = ['digital-art-1.html', 'digital-art-2.html', 'digital-art-3.html', 'digital-art-4.html', 'digital-art-5.html', 'digital-art-6.html', 'digital-art-7.html']; function randomSite() {var i = parseInt(Math.random() * sites.length); location.href = sites[i];}
